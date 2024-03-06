e6c02ffb262e87c29c77389199bbfa8af424d6b0
문준형 <moonabcd@naver.com>
2024-03-06 10:51:57 +0900

    add : 프로젝트 구조이해용 임시파일

---
+++ backend/src/main/java/com/tfeo/backend/common/config/SecurityConfig.java
@@ -0,0 +1,5 @@
+package com.tfeo.backend.common.config;
+
+public class SecurityConfig {
+	//프로젝트 구조이해를 위한 임시파일
+}
---
+++ backend/src/main/java/com/tfeo/backend/domain/home/controller/HomeController.java
@@ -0,0 +1,5 @@
+package com.tfeo.backend.domain.home.controller;
+
+public class HomeController {
+	//프로젝트 구조이해를 위한 임시파일
+}
