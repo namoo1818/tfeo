361c435ab2f45fe80eb260e864d60ab26a43da38
Your Name <you@example.com>
2024-03-08 17:55:46 +0900

    feat : 리뷰정보 확인기능 추가

--- data/test/convert_response_to_json.py
+++ data/test/convert_response_to_json.py
@@ -12,9 +12,10 @@ def main():
     # 여기에 API 호출을 위한 URL request를 입력
     url = "https://apis.zigbang.com/v2/aparts/items?domain=zigbang&geohash={wydm}"
     url2 = "https://apis.zigbang.com/apt/locals/prices/on-danjis?minPynArea=10%ED%8F%89%EC%9D%B4%ED%95%98&maxPynArea=60%ED%8F%89%EB%8C%80%EC%9D%B4%EC%83%81&geohash=wydm3"
+    url3 = "https://apis.zigbang.com/v2/aparts/items?domain=zigbang&geohash=wydm"
     # convert(url)
-    convert(url2)
-
+    # convert(url2)
+    convert(url3)
 
 if __name__ == "__main__":
     main()
