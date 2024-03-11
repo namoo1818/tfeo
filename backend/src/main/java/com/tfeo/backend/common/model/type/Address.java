df336c8a974013316dc9ecf4b5411697f456ed3d
김태윤 <whddn_0426@naver.com>
2024-03-11 17:45:20 +0900

    mod: Address 클래스 lombok 추가

--- backend/src/main/java/com/tfeo/backend/common/model/type/Address.java
+++ backend/src/main/java/com/tfeo/backend/common/model/type/Address.java
@@ -2,12 +2,20 @@ package com.tfeo.backend.common.model.type;
 
 import javax.persistence.Embeddable;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 @Embeddable
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
 	private String si;
 	private String sgg;
 	private String emd;
 	private String ro;
 	private String detail;
-
 }
