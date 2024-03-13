c7537051081ed44bec1260dee4f14f4e23f0f395
박중현 <wndgus0712@gmail.com>
2024-03-13 17:37:13 +0900

    feat: 메인페이지 UI 제작, 라우팅 설정
    Former-commit-id: 14b245319b8740409041a31342cbeb9ddc60555e

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import MyInfo from './pages/MyInfo';
 
 function App() {
   return (
     <div className="App">
-      <h2>A707</h2>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/my-info" element={<MyInfo />} />
+          {/* ... 여기에 더 많은 라우트를 추가할 수 있습니다 ... */}
+        </Routes>
+      </Router>
     </div>
   );
 }
