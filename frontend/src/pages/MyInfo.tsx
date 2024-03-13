8033ee5b45f59ab2850e14225d2398233bed903e
박중현 <wndgus0712@gmail.com>
2024-03-13 17:50:26 +0900

    feat: 메잎ì인페이지 제작, 라우팅 설정
    Former-commit-id: 11afbf1f1fa93d51c6fc260fd215b0d5857db03c

---
+++ frontend/src/pages/MyInfo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyInfo: React.FC = () => {
+  return (
+    <div>
+      <h1>내 정보</h1>
+      {/* ... 내 정보 페이지의 내용 ... */}
+    </div>
+  );
+};
+
+export default MyInfo;
