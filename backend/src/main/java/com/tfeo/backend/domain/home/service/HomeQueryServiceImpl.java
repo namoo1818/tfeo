e82c995e4a0b76cf725a42008a5ffd0b92c78c57
김태윤 <whddn_0426@naver.com>
2024-03-18 15:50:22 +0900

    refactor: HomeQueryServiceImpl RequiredArgsConstructor 추가
    Former-commit-id: a500b0dfd386f5b936b9dac9600c4a70c87f1daf

--- backend/src/main/java/com/tfeo/backend/domain/home/service/HomeQueryServiceImpl.java
+++ backend/src/main/java/com/tfeo/backend/domain/home/service/HomeQueryServiceImpl.java
@@ -3,8 +3,11 @@ package com.tfeo.backend.domain.home.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class HomeQueryServiceImpl implements HomeQueryService {
 	@Override
 	public void findHomeList() {
