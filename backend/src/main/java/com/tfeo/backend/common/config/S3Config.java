661edc0dc0f4e27b09e29cd5645874719e81727c
이하은 <hahi1108@gmail.com>
2024-03-18 13:38:01 +0900

    feat : s3 객체 생성 및 bean 등록
    Former-commit-id: 7779ecdbbf5c455efe6da06b1dfd56b2d7a89799

--- backend/src/main/java/com/tfeo/backend/common/config/S3Config.java
+++ backend/src/main/java/com/tfeo/backend/common/config/S3Config.java
@@ -2,8 +2,15 @@ package com.tfeo.backend.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 @Configuration
 @PropertySource("classpath:application-s3.yml")
@@ -15,4 +22,19 @@ public class S3Config {
 	private String secretKey;
 	@Value("${region.static}")
 	private String region;
+	@Bean
+	@Primary
+	public BasicAWSCredentials awsCredentialsProvider(){
+		BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+		return basicAWSCredentials;
+	}
+
+	@Bean
+	public AmazonS3 amazonS3() {
+		AmazonS3 s3Builder = AmazonS3ClientBuilder.standard()
+			.withRegion(region)
+			.withCredentials(new AWSStaticCredentialsProvider(awsCredentialsProvider()))
+			.build();
+		return s3Builder;
+	}
 }
