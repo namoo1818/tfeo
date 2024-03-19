d87b6950edf67201e608f4add9d1a38e9cdf80c9
문준형 <moonabcd@naver.com>
2024-03-20 00:09:53 +0900

    fix : pipeline test - jenkinsfile 수정9
    Former-commit-id: 751f59d0f2769002373d8f02d55ed151a9e382c7

--- exec/docker/initial/init.sql
+++ exec/docker/initial/init.sql
@@ -1,29 +1,11 @@
+CREATE DATABASE IF NOT EXISTS tfeo;
+
 CREATE DATABASE IF NOT EXISTS tfeo_test;
 CREATE DATABASE IF NOT EXISTS tfeo_temp;
 
-create user 'tfeo'@'%' identified by 'tfeo123';
-grant all privileges on `dotori_test`.* to 'tfeo'@'%';
-grant all privileges on `dotori_temp`.* to 'tfeo'@'%';
-flush privileges;
+CREATE USER 'tfeo'@'%' IDENTIFIED BY 'tfeo123';
 
+GRANT ALL PRIVILEGES ON `tfeo_test`.* TO 'tfeo'@'%';
+GRANT ALL PRIVILEGES ON `tfeo_temp`.* TO 'tfeo'@'%';
 
-DELIMITER //
-CREATE PROCEDURE GrantPrivilegesIfUserExists(IN user_name VARCHAR(10), IN db_name VARCHAR(10))
-BEGIN
-    DECLARE user_exists INT;
-
-SELECT COUNT(*)
-INTO user_exists
-FROM mysql.user
-WHERE user = user_name;
-
-IF user_exists > 0 THEN
-        SET @grant_query =
-                CONCAT('GRANT ALL PRIVILEGES ON `', db_name, '`.* TO\'', user_name, '\'@\'', host_name, '\'');
-PREPARE stmt FROM @grant_query;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
 FLUSH PRIVILEGES;
-END IF;
-END //
-
