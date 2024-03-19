b2f4bb911eb83fdabd43d6b0a3155cdafd1894ba
김태윤 <whddn_0426@naver.com>
2024-03-19 10:25:29 +0900

    add: redis 설정 파일 추가
    Former-commit-id: d1002a89f10ca07edc731f8fabb13db4e1ece3a5

---
+++ backend/src/main/java/com/tfeo/backend/common/config/RedisUtils.java
@@ -0,0 +1,27 @@
+package com.tfeo.backend.common.config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisUtils {
+	private final RedisTemplate<String, Object> redisTemplate;
+
+	public RedisUtils(RedisTemplate<String, Object> redisTemplate) {
+		this.redisTemplate = redisTemplate;
+	}
+
+	public void setData(String key, String value, Long expiredTime) {
+		redisTemplate.opsForValue().set(key, value, expiredTime, TimeUnit.SECONDS);
+	}
+
+	public String getData(String key) {
+		return (String)redisTemplate.opsForValue().get(key);
+	}
+
+	public void deleteData(String key) {
+		redisTemplate.delete(key);
+	}
+}
