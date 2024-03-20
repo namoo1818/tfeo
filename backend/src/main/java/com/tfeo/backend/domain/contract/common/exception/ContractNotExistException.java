177908000b717aec0839b1d0681448e49795d86f
3dlalswl3 <3dlalswl3@naver.com>
2024-03-20 16:04:01 +0900

    design : 회원 마이페이지 수정
    Former-commit-id: 651174626a23b0f66183a3379127fe5f5201893f

--- backend/src/main/java/com/tfeo/backend/domain/contract/common/exception/ContractNotExistException.java
+++ backend/src/main/java/com/tfeo/backend/domain/contract/common/exception/ContractNotExistException.java
@@ -3,7 +3,7 @@ package com.tfeo.backend.domain.contract.common.exception;
 import com.tfeo.backend.common.exception.BadRequestException;
 
 public class ContractNotExistException extends BadRequestException {
-	private static final String MESSAGE = "존재하지 않는 계약입니다. contractNo: %d";
+	private static final String MESSAGE = "존재하지 않는 계약입니다. : %d";
 	public ContractNotExistException(Long contractNo) {
 		super(String.format(MESSAGE, contractNo));
 	}
--- frontend/src/pages/MyInfo.tsx
+++ frontend/src/pages/MyInfo.tsx
@@ -1,45 +1,109 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import Footer from '../components/footer/Footer';
 import '../styles/MyInfo.css';
+import { Button, Box, Avatar } from '@mui/material';
 
 const MyInfo: React.FC = () => {
-  // 이미지 상태를 관리하기 위한 state 추가
-  const [profileImage, setProfileImage] = useState('/test/profile.png'); // 기본 이미지 경로 설정
+  const [profileImage, setProfileImage] = useState('/test/profile.png');
 
-  // 이미지 업로드 핸들러 함수
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
-      // 이미지를 상태에 저장합니다 (예: URL.createObjectURL을 사용)
       setProfileImage(URL.createObjectURL(event.target.files[0]));
     }
   };
 
+  //화면에 출력되는 파일
+  const [selectedImages, setSelectedImages] = useState([]);
+  //서버에 보내지는 파일
+  const [selectedFiles, setSelectedFiles] = useState(null as any);
+
+  const onSelectFile = (e: any) => {
+    e.preventDefault();
+    e.persist();
+    //선택한 파일
+    const selectedFiles = e.target.files;
+    //선택한 파일들을 fileUrlList에 넣어준다.
+    const fileUrlList = [...selectedFiles];
+
+    // 업로드되는 파일에는 url이 있어야 한다. filePath로 보내줄 url이다.
+    //획득한 Blob URL Address를 브라우져에서 그대로 호출 시에 이미지는 표시가 되고 ,
+    //일반 파일의 경우 다운로드를 할 수 있다.
+    for (let i = 0; i < selectedFiles.length; i++) {
+      const nowUrl = URL.createObjectURL(selectedFiles[i]);
+      fileUrlList.push(nowUrl[i]);
+    }
+
+    setSelectedFiles(fileUrlList);
+
+    //Array.from() 은 문자열 등 유사 배열(Array-like) 객체나 이터러블한 객체를 배열로 만들어주는 메서드이다.
+    const selectedFileArray: any = Array.from(selectedFiles);
+
+    //브라우저 상에 보여질 파일 이름
+    const imageArray = selectedFileArray.map((file: any) => {
+      return file.name;
+    });
+
+    // 첨부파일 삭제시
+    setSelectedImages((previousImages: any) => previousImages.concat(imageArray));
+    e.target.value = '';
+  };
+
+  //브라우저상에 보여질 첨부파일
+  const attachFile =
+    selectedImages &&
+    selectedImages.map((image: any) => {
+      return (
+        <div key={image}>
+          <div>{image}</div>
+          <button onClick={() => setSelectedImages(selectedImages.filter((e) => e !== image))}></button>
+        </div>
+      );
+    });
+
+  const data = [
+    { label: '김싸피', description: '이름' },
+    { label: '여자', description: '성별' },
+    { label: '25세', description: '나이' },
+    { label: '싸피대학교', description: '대학교' },
+    { label: '01012345678', description: '전화번호' },
+  ];
+
   return (
-    <div>
-      <div className="background-image-container">
-        <img src="/assets/mainLogo.png" alt="로고 이미지" />
-      </div>
+    <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
       <div className="profile-image-container">
-        <img src={profileImage} alt="프로필 이미지" className="profile-image" />
-        <button className="image-upload-button">
-          <label htmlFor="image-upload">사진 등록</label>
-          <input
-            type="file"
-            id="image-upload"
-            style={{ display: 'none' }} // 파일 입력 숨기기
-            onChange={handleImageUpload}
-          />
-        </button>
+        <label htmlFor="image-upload">
+          <Avatar alt="Remy Sharp" src={profileImage} sx={{ width: 150, height: 150, marginTop: 5 }} />
+        </label>
+        <input type="file" id="image-upload" style={{ display: 'none' }} onChange={handleImageUpload} />
       </div>
-      <div className="info-buttons-container">
-        <Link to="/apply-list">집신청 리스트</Link>
-        <Link to="/activity-certification">활동인증</Link>
-        <Link to="/edit-profile">내정보수정</Link>
-        <Link to="/view-contract">계약서보기</Link>
-        <Link to="/register-certificate">재학증명서 등록</Link>
-        <Link to="/current-home-info">현재 집정보</Link>
+      <div>
+        {data.map((item, index) => (
+          <React.Fragment key={index}>
+            <span>{item.description}</span>
+            <Box
+              height={20}
+              width={300}
+              my={1}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              gap={4}
+              p={2}
+              sx={{ border: '1px solid #79747E', borderRadius: '5px' }}
+            >
+              {item.label}
+            </Box>
+          </React.Fragment>
+        ))}
+        <br />
+        <span>재학증명서</span>{' '}
+        <Button variant="outlined" onClick={() => setSelectedImages(selectedImages.filter((e) => e !== null))}>
+          다운로드
+        </Button>{' '}
+        <span>2024.09.20 만료</span>
       </div>
+      <br />
+      <Button variant="outlined">수정하기</Button>
       <Footer />
     </div>
   );
