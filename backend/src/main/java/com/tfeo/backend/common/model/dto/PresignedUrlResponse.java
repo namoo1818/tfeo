76e617a628f413c2f8ae62519aac563e0a9d3c7b
이하은 <hahi1108@gmail.com>
2024-03-20 13:57:08 +0900

    rename : presigned url response dto 클래스명 수정
    Former-commit-id: f59b4fd7e7f1ced506076a5e6af29814f1336892

--- backend/src/main/java/com/tfeo/backend/common/model/dto/PresignedUrlResponse.java
+++ backend/src/main/java/com/tfeo/backend/common/model/dto/PresignedUrlResponse.java
@@ -3,11 +3,10 @@ package com.tfeo.backend.common.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 @Builder
-public class PresignedUrlDto {
+public class PresignedUrlResponse {
 	String url;
 }
