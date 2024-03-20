cbde65ca827003e18aa374a120da0e12ac6ca242
3dlalswl3 <3dlalswl3@naver.com>
2024-03-20 16:22:59 +0900

    style: 파일 입출력 코드 변경
    Former-commit-id: b96ba280527ac31f01220e420566017e0e26100b

--- frontend/src/pages/MyInfo.tsx
+++ frontend/src/pages/MyInfo.tsx
@@ -12,54 +12,6 @@ const MyInfo: React.FC = () => {
     }
   };
 
-  //화면에 출력되는 파일
-  const [selectedImages, setSelectedImages] = useState([]);
-  //서버에 보내지는 파일
-  const [selectedFiles, setSelectedFiles] = useState(null as any);
-
-  const onSelectFile = (e: any) => {
-    e.preventDefault();
-    e.persist();
-    //선택한 파일
-    const selectedFiles = e.target.files;
-    //선택한 파일들을 fileUrlList에 넣어준다.
-    const fileUrlList = [...selectedFiles];
-
-    // 업로드되는 파일에는 url이 있어야 한다. filePath로 보내줄 url이다.
-    //획득한 Blob URL Address를 브라우져에서 그대로 호출 시에 이미지는 표시가 되고 ,
-    //일반 파일의 경우 다운로드를 할 수 있다.
-    for (let i = 0; i < selectedFiles.length; i++) {
-      const nowUrl = URL.createObjectURL(selectedFiles[i]);
-      fileUrlList.push(nowUrl[i]);
-    }
-
-    setSelectedFiles(fileUrlList);
-
-    //Array.from() 은 문자열 등 유사 배열(Array-like) 객체나 이터러블한 객체를 배열로 만들어주는 메서드이다.
-    const selectedFileArray: any = Array.from(selectedFiles);
-
-    //브라우저 상에 보여질 파일 이름
-    const imageArray = selectedFileArray.map((file: any) => {
-      return file.name;
-    });
-
-    // 첨부파일 삭제시
-    setSelectedImages((previousImages: any) => previousImages.concat(imageArray));
-    e.target.value = '';
-  };
-
-  //브라우저상에 보여질 첨부파일
-  const attachFile =
-    selectedImages &&
-    selectedImages.map((image: any) => {
-      return (
-        <div key={image}>
-          <div>{image}</div>
-          <button onClick={() => setSelectedImages(selectedImages.filter((e) => e !== image))}></button>
-        </div>
-      );
-    });
-
   const data = [
     { label: '김싸피', description: '이름' },
     { label: '여자', description: '성별' },
@@ -97,7 +49,7 @@ const MyInfo: React.FC = () => {
         ))}
         <br />
         <span>재학증명서</span>{' '}
-        <Button variant="outlined" onClick={() => setSelectedImages(selectedImages.filter((e) => e !== null))}>
+        <Button variant="outlined" onClick={() => null}>
           다운로드
         </Button>{' '}
         <span>2024.09.20 만료</span>
