33682c2307bb5a22cd152500d39a4b3e1d26c767
김태윤 <whddn_0426@naver.com>
2024-03-22 16:04:17 +0900

    feat: 계약 페이지 집 카드 정보 ui 임시 구현 * props만 매칭시켜놓고, 글만 작성해놨다
    Former-commit-id: 6f0d1b4d22c3ba3b36b615f68df1917aeb9aded2

--- frontend/src/components/contract/ContractHomeCard.tsx
+++ frontend/src/components/contract/ContractHomeCard.tsx
@@ -1,61 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import { IHomeContractCard } from '../../interfaces/HomeInterface';
+import { IHomeDetail, IHome } from '../../interfaces/HomeInterface';
 import { Card, CardContent, CardMedia } from '@mui/material';
+import { getRoadNameAddress, getEMDNameAddress } from '../../utils/addressUtils';
+import { IAddress } from '../../interfaces/AddressInterface';
+import home from '../../pages/Home';
+import { getRent } from '../../utils/moneyUtils';
+import { getDay, getMonth, getYear } from '../../utils/timeUtils';
+import { IContract } from '../../interfaces/ContractInterface';
+import '../../styles/ContractCard.css';
 
-const ContractHomeCard = () => {
-  const [homeContractCard, setHomeContractCard] = useState<IHomeContractCard>();
-  // 더미용
-  useEffect(() => {
-    setHomeContractCard({
-      homeNo: 1,
-      address: {
-        si: '서울특별시',
-        sgg: '강남구',
-        emd: '역삼동',
-        ro: '테헤란로 212',
-        detail: '멀티캠퍼스 1501호',
-      },
-      hostName: '김태윤',
-      rent: 200000,
-      startAt: '2024-03-19',
-      expiredAt: '2024-09-18',
-      progress: 'applied',
-      hostPersonality: {
-        smoke: false,
-        pet: false,
-        clean: false,
-        daytime: false,
-        nighttime: false,
-        extrovert: false,
-        introvert: false,
-        cold: true,
-        hot: false,
-        noTouch: true,
-      },
-      hostBank: '하나은행',
-      hostAccountNo: '2345-2345-123456',
-      hostImageList: ['hostImage1.url', 'hostImage2.url'],
-      homeImageList: ['homeImage1.url', 'homeImage2.url', 'homeImage3.url'],
-    });
-  }, []);
-  const show = () => {
-    console.log(homeContractCard);
-  };
-  const getAddress = () => {
-    if (!homeContractCard) return '';
-    const address = homeContractCard.address;
-    return address.si + ' ' + address.sgg + ' ' + address.emd;
-  };
+interface Props {
+  status: string;
+  homeDetail: IHomeDetail;
+  contract: IContract;
+}
+const ContractHomeCard = ({ status, homeDetail, contract }: Props) => {
+  const { home, homeOption, hostPersonality } = homeDetail;
   return (
     <div className="homeCard">
       <h2>계약 중인 집 카드</h2>
-      <div>status</div>
-      <button onClick={show}>콘솔 찍기</button>
       <Card>
         <CardMedia component="img" image="/assets/mainLogo.png" />
-        <CardContent>
-          <div>{homeContractCard?.hostName}</div>
-          <div>{getAddress()}</div>
+        <CardContent className="cardContent">
+          <div>{status === 'completed' ? '태그보여줘야함' : ''}</div>
+          <div>{status === 'applied' ? getEMDNameAddress(home.address) : getRoadNameAddress(home.address)}</div>
+          <div>
+            {home.hostName} {home.hostGender === 'M' ? '할아버지' : '할머니'}
+          </div>
+          <div>월 {getRent(home.rent)}만원</div>
+          <div>
+            기간: {getYear(contract.startAt)}년 {getMonth(contract.startAt)}월 {getDay(contract.startAt)}일 ~{' '}
+            {getYear(contract.expiredAt)}년 {getMonth(contract.expiredAt)}월 {getDay(contract.expiredAt)}일
+          </div>
         </CardContent>
       </Card>
     </div>
