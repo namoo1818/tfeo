c3a00bc078c1238ed42e0ee9745864168816044a
이하은 <hahi1108@gmail.com>
2024-03-22 13:47:50 +0900

    mod : 메서드 파라미터 수정
    Former-commit-id: a6d6e1fdb17706329d0d988deb6f66ce1586a4d2

--- backend/src/main/java/com/tfeo/backend/common/service/FileService.java
+++ backend/src/main/java/com/tfeo/backend/common/service/FileService.java
@@ -3,6 +3,6 @@ package com.tfeo.backend.common.service;
 public interface FileService {
 
 	String createPresignedUrlToUpload(String prefix);
-	String createPresignedUrlToDownload(String prefix);
+	String createPresignedUrlToDownload(String filePath);
 
 }
