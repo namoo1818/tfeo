3c15227a18a783542b103653b20a915f670e48ee
이하은 <hahi1108@gmail.com>
2024-03-22 10:19:01 +0900

    feat : 최종 home, home_option 관련 칼럼 테이블 완성
    Former-commit-id: 5e5599100613cffce2b062cf77b901b4d87856e2

--- data/preprocessing/naver/processing/naver_home.py
+++ data/preprocessing/naver/processing/naver_home.py
@@ -3,7 +3,7 @@ import requests
 
 # kakao api (도로명주소)
 url = "https://dapi.kakao.com/v2/local/geo/coord2address.json"
-REST_API_KEY = ""
+REST_API_KEY = "b5d75b7ac5b0138157e2ee46717e396c"
 headers = {"Authorization": "KakaoAK {}".format(REST_API_KEY)}
 
 file = open('../data/naver_data_final_utf8.csv', 'r', encoding='UTF8')
@@ -30,14 +30,17 @@ for line in fReader:
         road_address = documents.get("road_address")
         if road_address is None:
             address = documents.get("address").get("address_name")
-            data[9] = address
+            data[0] = address.get("region_1depth_name")
+            data[1] = address.get("region_2depth_name")
+            data[2] = address.get("region_3depth_name")
+            data[9] = address.get("address_name")
         else:
             data[0] = road_address.get("region_1depth_name")
             data[1] = road_address.get("region_2depth_name")
             data[2] = road_address.get("region_3depth_name")
             data[3] = road_address.get("road_name") + " " + road_address.get("main_building_no")
-
-    data[4] = line[1] + "층"
+            data[9] = road_address.get("address_name")
+    data[4] = line[1]
     data[5] = line[2]
     data[6] = line[5]
     data[7] = line[6]
@@ -47,7 +50,7 @@ file.close()
 print(data_csv)
 
 
-newFile = open('../data/naver_home_utf8.csv', 'w', encoding='UTF8', newline='')
+newFile = open('../data/naver_home_utf8=.csv', 'w', encoding='UTF8', newline='')
 writer = csv.writer(newFile)
 writer.writerows(data_csv)
 newFile.close()
