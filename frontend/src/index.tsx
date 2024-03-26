400fd7bc4656661c57a5136036e6e1e72af26043
김태윤 <whddn_0426@naver.com>
2024-03-26 11:43:57 +0900

    remove: Tailwind css 삭제
    Former-commit-id: 5561bb7a67d7cf5883088f8e969b7564ce854a45

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { theme } from './styles/Theme';
 import { ThemeProvider } from '@mui/material/styles';
