05d7dba35794da03ed75bc9c96c10238fdf19f85
김태윤 <whddn_0426@naver.com>
2024-03-26 10:12:21 +0900

    remove: 안 쓰는 dto 삭제
    Former-commit-id: 61e8b32adc0108a51cae9b77eb08adac5d05cb06

---
+++ backend/src/main/java/com/tfeo/backend/domain/member/repository/MemberPersonalityRepository.java
@@ -0,0 +1,8 @@
+package com.tfeo.backend.domain.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tfeo.backend.common.model.entity.MemberPersonality;
+
+public interface MemberPersonalityRepository extends JpaRepository<MemberPersonality, Long> {
+}
