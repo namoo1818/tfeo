6301a1010443159175241b97ac32dd5b3027559a
문준형 <moonabcd@naver.com>
2024-03-27 11:36:50 +0900

    rebase : 3
    Former-commit-id: e540eea97dc37a216518f6ae17025c8094bebd13

--- backend/src/main/java/com/tfeo/backend/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/tfeo/backend/domain/member/controller/MemberController.java
@@ -1,7 +1,18 @@
 package com.tfeo.backend.domain.member.controller;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,19 +32,72 @@ import com.tfeo.backend.domain.member.model.dto.MemberResponseDto;
 import com.tfeo.backend.domain.member.model.dto.SmsRequestDto;
 import com.tfeo.backend.domain.member.model.dto.SmsVerifyDto;
 import com.tfeo.backend.domain.member.model.dto.SurveyRequestDto;
+import com.tfeo.backend.domain.member.repository.MemberRepository;
+import com.tfeo.backend.domain.member.service.JwtService;
 import com.tfeo.backend.domain.member.service.MemberService;
+import com.tfeo.backend.domain.member.service.RedisService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
 public class MemberController {
 
+	private final JwtService jwtService;
+	private final MemberRepository memberRepository;
+	private final RedisService redisService;
 	private final MemberService memberService;
 
 	//auth 적용 이전 임시
 	private final Long temporaryMemberNo = 1L;
+
+	@GetMapping("/status")
+	public ResponseEntity<?> getAuthStatus() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		boolean isAnonymous = authentication instanceof AnonymousAuthenticationToken;
+		boolean isAuthenticated = authentication != null && authentication.isAuthenticated() && !isAnonymous;
+
+		return ResponseEntity.ok(Collections.singletonMap("isAuthenticated", isAuthenticated));
+	}
+
+	@GetMapping("/reaccesstoken")
+	public ResponseEntity<?> reAccessToken(HttpServletRequest request, HttpServletResponse response) throws
+		IOException {
+
+		String refreshToken = jwtService.extractRefreshToken(request).orElse(null);
+
+		Optional<String> jwtemail = jwtService.extractEmailFromRefreshToken(request);
+		Optional<String> jwtrole = jwtService.extractroleFromRefreshToken(request);
+		String email = jwtemail.get();
+		String role = jwtrole.get();
+		log.info("role:{}", role);
+
+		String redisrefreshToken = redisService.getRefreshToken(email).get();
+		log.info("refreshToken:{}", refreshToken);
+		log.info("redisrefreshToken:{}", redisrefreshToken);
+		if (redisrefreshToken != null) {
+			if (redisService.isBlacklisted(redisrefreshToken)) {
+				response.sendError(HttpStatus.UNAUTHORIZED.value(), "만료된 refresh 토큰입니다. 다시로그인해주세요");
+			} else {
+				if (refreshToken.equals(redisrefreshToken)) {
+					String accessToken = jwtService.createAccessToken(email, "USER"); //서비스확장시 role 부여수정필요
+					jwtService.sendAccessToken(response, accessToken);
+					return ResponseEntity.ok("accessToken:" + accessToken);
+				} else {
+					return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+						.body("유효하지않은 접근입니다. 다시 로그인해주세요.");
+				}
+			}
+		} else {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+				.body("만료된 refresh 토큰입니다. 다시 로그인해주세요.");
+		}
+		return ResponseEntity.ok("ok");
+	}
 
 	//회원 상세정보 조회
 	@GetMapping("")
