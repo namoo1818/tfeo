a4ffb856d0a53e7fe13d02484e96312dc1b34d8d
김태윤 <whddn_0426@naver.com>
2024-03-28 00:45:04 +0900

    feat: s3, 계약 관련 api axios 함수 생성
    Former-commit-id: 91b2def2377dbda28fbe800c0dacb47e6af39177

--- frontend/src/api/ContractApis.ts
+++ frontend/src/api/ContractApis.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import { ISuccessResponse } from '../interfaces/SuccessResponseInterface';
 import { customAxios } from './customAxios';
+import { IContractForm, IContractInfo } from '../interfaces/ContractInterface';
 
 // pdf 생성
 export const createForm = async (homeNo: number) => {
   try {
-    const response = await customAxios.get<ISuccessResponse>(`/contracts/creation-form/${homeNo}`);
+    const response = await customAxios.get<ISuccessResponse>(`/api/contracts/creation-form/${homeNo}`);
     return response.data.result;
   } catch (e) {
     console.log(e);
@@ -20,10 +21,42 @@ export const applyApproval = async (homeNo: number, memberNo: number) => {
     memberNo: memberNo,
   };
   try {
-    const response = await customAxios.put<ISuccessResponse>(`/home/apply-approval`, request);
-    console.log(response.data.result);
+    const response = await customAxios.put<ISuccessResponse>(`/api/home/apply-approval`, request);
+    console.log(response.data.message);
+    return response.data.result as IContractForm;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+export const signContract = async (contractNo: number) => {
+  try {
+    const response = await customAxios.put<ISuccessResponse>(`/api/contracts/sign/${contractNo}`);
+    console.log(response.data.message);
+    return response.data.result as string; // presigned url 반환
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getContractFormPreSignedUrl = async (contractNo: number) => {
+  try {
+    const response = await customAxios.get<ISuccessResponse>(`/api/contracts/contract-url/${contractNo}`);
+    console.log(response.data.message);
+    return response.data.result as string; //pdf 조회용 presigned url 반환
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// 학생이 신청한 계약 조회 /members/home
+export const getMemberContract = async () => {
+  try {
+    const response = await customAxios.get<ISuccessResponse>('/api/members/home');
     return response.data.result;
   } catch (e) {
+    // 학생이 신청한 계약이 없는 경우가 고려되어야 한다. 신청한 계약 없을 경우 계약 없음 페이지 띄워야함
     console.log(e);
     throw e;
   }
--- frontend/src/api/customAxios.ts
+++ frontend/src/api/customAxios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 export const customAxios: AxiosInstance = axios.create({
-  baseURL: `${process.env.API_URL}`, // 기본 서버 주소 입력
+  baseURL: process.env.REACT_APP_API_URL, // 기본 서버 주소 입력
   headers: {
     // withCredentials: true,
   },
