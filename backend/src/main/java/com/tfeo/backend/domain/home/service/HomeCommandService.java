9d5aa751a738c5d560f9ac11f3674861a8a54e0d
문준형 <moonabcd@naver.com>
2024-03-27 09:26:42 +0900

    fix : public제거 자동수정
    Former-commit-id: 5ca4f6e664c4fc8df8cf712720cc1a445c7c1f24

--- backend/src/main/java/com/tfeo/backend/domain/home/service/HomeCommandService.java
+++ backend/src/main/java/com/tfeo/backend/domain/home/service/HomeCommandService.java
@@ -7,19 +7,19 @@ import com.tfeo.backend.domain.home.model.dto.HomeRequestDto;
 import com.tfeo.backend.domain.home.model.dto.HomeResponseDto;
 
 public interface HomeCommandService {
-	public HomeResponseDto addHome(HomeRequestDto homeRequestDto);
+	HomeResponseDto addHome(HomeRequestDto homeRequestDto);
 
-	public void addHomeAppliedByNoneMember(HomeDto homeDto);
+	void addHomeAppliedByNoneMember(HomeDto homeDto);
 
-	public HomeResponseDto modifyHome(Long homeNo, HomeRequestDto homeRequestDto);
+	HomeResponseDto modifyHome(Long homeNo, HomeRequestDto homeRequestDto);
 
-	public void removeHome(Long homeNo);
+	void removeHome(Long homeNo);
 
-	public void approveHomeNoneMemberRegistration(Long homeNo);
+	void approveHomeNoneMemberRegistration(Long homeNo);
 
-	public void refuseHomeNoneMemberRegistration(Long homeNo);
+	void refuseHomeNoneMemberRegistration(Long homeNo);
 
-	public CreateFormResponseDto approveHomeApplication(HomeApplicationRequestDto homeApplicationRequest);
+	CreateFormResponseDto approveHomeApplication(HomeApplicationRequestDto homeApplicationRequest);
 
-	public void refuseHomeApplication(HomeApplicationRequestDto homeApplicationRequest);
+	void refuseHomeApplication(HomeApplicationRequestDto homeApplicationRequest);
 }
