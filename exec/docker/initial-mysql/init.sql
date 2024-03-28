21b5fd42141b341c43c11de0dca912a4061d8bfa
문준형 <moonabcd@naver.com>
2024-03-29 01:58:39 +0900

    chore : build request
    Former-commit-id: 0af4b29f97fc1dbc36531303e8d8ce85bd52170e

--- exec/docker/initial-mysql/init.sql
+++ exec/docker/initial-mysql/init.sql
@@ -7,7 +7,6 @@ CREATE DATABASE IF NOT EXISTS tfeo_test;
 CREATE DATABASE IF NOT EXISTS tfeo_temp;
 
 
-
 GRANT ALL PRIVILEGES ON `tfeo`.* TO 'tfeo'@'%';
 GRANT ALL PRIVILEGES ON `tfeo_test`.* TO 'tfeo'@'%';
 GRANT ALL PRIVILEGES ON `tfeo_temp`.* TO 'tfeo'@'%';
