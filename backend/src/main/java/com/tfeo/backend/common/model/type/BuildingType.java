ba0b46d954949467f931db0766c53720f9912c52
ê김태윤 <whddn_0426@naver.com>
2024-03-29 17:39:41 +0900

    fix: BuildingType enum 수정
    Former-commit-id: e987b4a98b366058ecf45b382c695f87e99cfcf9

--- backend/src/main/java/com/tfeo/backend/common/model/type/BuildingType.java
+++ backend/src/main/java/com/tfeo/backend/common/model/type/BuildingType.java
@@ -1,10 +1,5 @@
 package com.tfeo.backend.common.model.type;
 
 public enum BuildingType {
-	APT,				// 아파트
-	OPST,				// 오피스텔
-	VL,					// 빌라
-	JT,					// 주택
-	DDDGG,				// 단독, 다가구
-	OR                  // 원룸
+	APT, OPST, VL, JT, DDDGG, OR
 }
