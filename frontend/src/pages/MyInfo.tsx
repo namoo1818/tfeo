ed261fc358b398cc520f58c95d7ce7a2c985cea7
김태윤 <whddn_0426@naver.com>
2024-03-31 14:19:48 +0900

    refactor: 내 정보 페이지 수정 중
    Former-commit-id: c7fa0f17b786bdd20702e5cc3e076de54bc0fa8f

--- frontend/src/pages/MyInfo.tsx
+++ frontend/src/pages/MyInfo.tsx
@@ -3,38 +3,21 @@ import { Button, Avatar } from '@mui/material';
 import { useMemberStore } from '../store/MemberStore';
 import Footer from '../components/footer/Footer';
 import { getMember, modifyMember } from '../api/MemberApis';
+import axios from 'axios';
+import SearchAddress from '../components/mypage/SearchAddress';
+import { IMember } from '../interfaces/MemberInterface';
+import { getFileFromS3 } from '../api/S3Apis';
 
 const MyInfo: React.FC = () => {
   const [isEditing, setIsEditing] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [newMemberInfo, setNewMemberInfo] = useState<IMember>();
   const { setMemberState, setAddressState, MemberInfo, AddressInfo } = useMemberStore();
-  const [newMemberInfo, setNewMemberInfo] = useState({
-    memberNo: MemberInfo.memberNo,
-    socialId: MemberInfo.socialId,
-    socialType: MemberInfo.socialType,
-    role: MemberInfo.role,
-    name: MemberInfo.name,
-    phone: MemberInfo.phone,
-    email: MemberInfo.email,
-    registerNo: MemberInfo.registerNo,
-    college: MemberInfo.college,
-    lat: MemberInfo.lat,
-    lng: MemberInfo.lng,
-    address: MemberInfo.address,
-    profileUrl: MemberInfo.profileUrl,
-    gender: MemberInfo.gender,
-    certificate: MemberInfo.certificate,
-    memberPersonalityNo: MemberInfo.memberPersonalityNo,
-    certificateStatus: MemberInfo.certificateStatus,
-    certificateRegisterDate: MemberInfo.certificateRegisterDate,
-    certificateExpirationDate: MemberInfo.certificateExpirationDate,
-    sleepAt: MemberInfo.sleepAt,
-    wakeAt: MemberInfo.wakeAt,
-    returnAt: MemberInfo.returnAt,
-    memberPersonality: MemberInfo.memberPersonality,
-  });
 
   // 프로필 업로드
-  const [profileImage, setProfileImage] = useState(newMemberInfo.profileUrl);
+  const [profileImage, setProfileImage] = useState('');
+  //재학증명서 업로드
+  const [ceritifcate, setCertificate] = useState('');
   const handleImageUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
       setProfileImage(URL.createObjectURL(event.target.files[0]));
@@ -51,13 +34,32 @@ const MyInfo: React.FC = () => {
     };
     fetchData();
   }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      if (MemberInfo.certificate && MemberInfo.certificate !== '') {
+        try {
+          const response = await getFileFromS3(MemberInfo.certificate);
+          if (response) {
+            setCertificate(URL.createObjectURL(response));
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    };
+    fetchData();
+  }, [MemberInfo]);
 
   const handleEdit = () => {
     setIsEditing(!isEditing);
   };
+  const handleModal = () => {
+    setOpen(!open);
+  };
 
   const handleMemberChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
     const { id, value } = event.target;
+    if (!newMemberInfo) return;
     setNewMemberInfo({
       ...newMemberInfo,
       [id]: value,
@@ -66,6 +68,7 @@ const MyInfo: React.FC = () => {
 
   const saveChanges = async () => {
     setIsEditing(false);
+    if (!newMemberInfo) return;
     await modifyMember(
       newMemberInfo.name,
       newMemberInfo.phone,
@@ -84,12 +87,26 @@ const MyInfo: React.FC = () => {
       [name]: value,
     });
   };
+  if (!newMemberInfo) {
+    return (
+      <>
+        <div>로그인이 필요합니다.</div>
+      </>
+    );
+  }
+  const showlog = () => {
+    console.log(newMemberInfo);
+  };
 
   return (
     <div
       className="wrap-container"
       style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}
+      onClick={() => {
+        if (open) setOpen(false);
+      }}
     >
+      <button onClick={showlog}>콘솔찍기</button>
       <div className="profile-image-container">
         <label htmlFor="image-upload">
           <Avatar alt="Remy Sharp" src={profileImage} sx={{ width: 100, height: 100, marginTop: 5 }} />
@@ -104,8 +121,8 @@ const MyInfo: React.FC = () => {
           <textarea
             id="name"
             style={{ width: '150%', height: '2em', border: 'none', resize: 'none', whiteSpace: 'pre-line' }}
-            defaultValue={MemberInfo.name}
-            readOnly
+            value={newMemberInfo.name}
+            onChange={handleMemberChange}
           />
           <br />
           <span>대학교</span>
@@ -113,7 +130,7 @@ const MyInfo: React.FC = () => {
           <textarea
             id="college"
             style={{ width: '150%', height: '2em', border: 'none', resize: 'none', whiteSpace: 'pre-line' }}
-            defaultValue={MemberInfo.college}
+            value={newMemberInfo.college}
             readOnly
           />
           <br />
@@ -122,7 +139,7 @@ const MyInfo: React.FC = () => {
           <textarea
             id="email"
             style={{ width: '150%', height: '2em', border: 'none', resize: 'none', whiteSpace: 'pre-line' }}
-            defaultValue={MemberInfo.email}
+            value={MemberInfo.email}
             readOnly
           />
           <br />
@@ -150,6 +167,7 @@ const MyInfo: React.FC = () => {
           <br />
           <span>주소</span>
           <br />
+          <SearchAddress open={open} handleModal={handleModal} />
           <textarea
             className={isEditing ? 'textarea-editing' : ''}
             id="address"
