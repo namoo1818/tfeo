7e9251712a85bc16ed3ac9b3a35c51716ffa57e5
이하은 <hahi1108@gmail.com>
2024-03-31 12:44:31 +0900

    feat : 헤더 필터 store 변수 도입
    Former-commit-id: a59737fb96ffb3137a2b4caf6eba11f4896b9c9e

--- frontend/src/components/home/Header.tsx
+++ frontend/src/components/home/Header.tsx
@@ -19,18 +19,7 @@ const Header: React.FC = () => {
     if (isMapLoaded) {
       setHeaderFilterChanged(true);
     }
-  }, [isMapLoaded, setVisibleHomes]); // mapLoaded에 의존하는 useEffect
-
-  const marks = [
-    {
-      value: 0,
-      label: '0만원',
-    },
-    {
-      value: 100,
-      label: '100만원',
-    },
-  ];
+  }, [isMapLoaded, setVisibleHomes]);
 
   const getDescriptionStyle = (filter: boolean): React.CSSProperties => {
     return filter ? { fontWeight: 'bold' } : {};
@@ -41,166 +30,50 @@ const Header: React.FC = () => {
     color: isActive ? '#000000' : theme.palette.primary.contrastText,
   });
 
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const ModalContainer = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
-
-  const ModalContent = styled.div`
-    background-color: white;
-    padding: 20px;
-    border-radius: 8px;
-  `;
-
-  const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    boxShadow: 24,
-    p: 4,
-  };
-
-  const [value, setValue] = React.useState<number[]>([0, 1000000]);
-
-  const handleChange = (event: Event, newValue: number | number[]) => {
-    setValue(newValue as number[]);
-  };
-
-  const handleOptionClick = (value: string) => {
-    toggleOption(value);
-  };
-
-  const handleTypeClick = (value: string) => {
-    toggleType(value);
-  };
-
-  const handleSearchClick = () => {
-    setModalOpen(false);
-    const fetchData = async () => {
-      try {
-        // 성향 반영 추천 api
-        const requestData = {
-          search_condition: search_condition,
-          member_personality: member_personality,
-        };
-        // http://j10a707.p.ssafy.io:8000/recommend
-        const response = await RecommendAxios.post('/recommend', requestData);
-        console.log(response.data);
-        setHomes(response.data); // 상태 업데이트
-        setIsMapLoaded(true); // 지도 로드 플래그 설정
-      } catch (error) {
-        console.error('집 리스트를 가져오는 데 실패했습니다 : ', error);
-      }
-    };
-    fetchData();
-  };
-
   const headerFilterClick = (filterKey: string) => {
-    if (filterKey == 'school') {
-      selectFilter({ school: !school });
-      // 학교 근처 필터링
-      const homeBySchool = visibleHomes.filter((home) => home.distance < 5);
-      setVisibleHomes(homeBySchool);
-      console.log('학교 근처 필터링', homeBySchool);
-      console.log('homes', homes);
-      console.log('visiblehomes', visibleHomes);
-    } else if (filterKey == 'subway') {
-      selectFilter({ subway: !subway });
-      // 역세권 필터링
-      const homesByStation = visibleHomes.filter((home) => home.station === 1);
-      setVisibleHomes(homesByStation);
-      toggleOption('station');
-      console.log('아파트 필터링', homesByStation);
-      console.log('homes', homes);
-      console.log('visiblehomes', visibleHomes);
-    } else if (filterKey == 'apartment') {
-      selectFilter({ apartment: !apartment });
-      const homesByType = visibleHomes.filter((home) => home.type === 'APT');
-      setVisibleHomes(homesByType);
-      console.log('아파트 필터링', homesByType);
-      console.log('homes', homes);
-      console.log('visiblehomes', visibleHomes);
-    } else if (filterKey == 'pets') {
-      selectFilter({ pets: !pets });
-      const homesByPet = visibleHomes.filter((home) => home.pet === 1);
-      setVisibleHomes(homesByPet);
-      console.log('반려동물 필터링', homesByPet);
-      console.log('homes', homes);
-      console.log('visibleHomes', visibleHomes);
-    }
-  };
-
-  const searchFilterClick = () => {
-    setModalOpen(true);
-    setSearchCondition({
-      internet: false,
-      gas: false,
-      washing_machine: false,
-      air_conditioner: false,
-      refrigerator: false,
-      elevator: false,
-      microwave: false,
-      toilet: false,
-      breakfast: false,
-      heating: false,
-      parking: false,
-      station: false,
-      move_in_date: false,
-      sink: false,
-      APT: false,
-      OPST: false,
-      VL: false,
-      JT: false,
-      DDDGG: false,
-      OR: false,
-      rent_max: 100,
-      rent_min: 0,
-      lat: 37.609641,
-      lng: 126.997697,
+    toggleFilter(filterKey);
+    setFilterCondition({
+      school: options[0].choice,
+      subway: options[1].choice,
+      apartment: options[2].choice,
+      pets: options[3].choice,
     });
+    setHeaderFilterChanged(true);
   };
 
   return (
     <header>
       <div className="criteria-container">
-        <div className="element-container" onClick={() => headerFilterClick('school')}>
-          <AccountBalanceIcon style={getIconStyle(school)} />
-          <span className="criteria-description" style={getDescriptionStyle(school)}>
-            학교 근처
+        <div className="element-container" onClick={() => headerFilterClick(filters[0].value)}>
+          <AccountBalanceIcon style={getIconStyle(filters[0].choice)} />
+          <span className="criteria-description" style={getDescriptionStyle(filters[0].choice)}>
+            {filters[0].option}
           </span>
         </div>
-        <div className="element-container" onClick={() => headerFilterClick('subway')}>
-          <DirectionsSubwayIcon style={getIconStyle(subway)} />
-          <span className="criteria-description" style={getDescriptionStyle(subway)}>
-            역세권
+        <div className="element-container" onClick={() => headerFilterClick(filters[1].value)}>
+          <DirectionsSubwayIcon style={getIconStyle(filters[1].choice)} />
+          <span className="criteria-description" style={getDescriptionStyle(filters[1].choice)}>
+            {filters[1].option}
           </span>
         </div>
-        <div className="element-container" onClick={() => headerFilterClick('apartment')}>
-          <ApartmentIcon style={getIconStyle(apartment)} />
-          <span className="criteria-description" style={getDescriptionStyle(apartment)}>
-            아파트
+        <div className="element-container" onClick={() => headerFilterClick(filters[2].value)}>
+          <ApartmentIcon style={getIconStyle(filters[2].choice)} />
+          <span className="criteria-description" style={getDescriptionStyle(filters[2].choice)}>
+            {filters[2].option}
           </span>
         </div>
-        <div className="element-container" onClick={() => headerFilterClick('pets')}>
-          <PetsIcon style={getIconStyle(pets)} />
-          <span className="criteria-description" style={getDescriptionStyle(pets)}>
-            반려동물
+        <div className="element-container" onClick={() => headerFilterClick(filters[3].value)}>
+          <PetsIcon style={getIconStyle(filters[3].choice)} />
+          <span className="criteria-description" style={getDescriptionStyle(filters[3].choice)}>
+            {filters[3].option}
           </span>
         </div>
         <div className="circle-border">
-          <TuneIcon onClick={searchFilterClick} fontSize="medium" style={{ color: '#A0A0A0', cursor: 'pointer' }} />
+          <TuneIcon
+            onClick={() => setModalOpen(true)}
+            fontSize="medium"
+            style={{ color: '#A0A0A0', cursor: 'pointer' }}
+          />
         </div>
         {modalOpen && <FilterModal modalOpen={modalOpen} setModalOpen={setModalOpen} />}
       </div>
