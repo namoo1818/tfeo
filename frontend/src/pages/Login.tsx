f33628337883548611aae785756b2374a95d7635
박중현 <wndgus0712@gmail.com>
2024-03-31 13:35:29 +0900

    add : 랜딩페이지 제작
    Former-commit-id: 24f39889c5fe498927a0431f3e468c83f771a85d

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,5 @@
-// const Header: React.FC = () => {
-//   return <div>Header Placeholder</div>;
-// };
-//
-// export default Header;
-
-// LoginPage.tsx
 import React from 'react';
+import '../styles/Login.css';
 
 const LoginPage: React.FC = () => {
   const redirectToAuthUrl = (url: string) => {
@@ -24,9 +18,60 @@ const LoginPage: React.FC = () => {
 
   return (
     <div>
-      <div>
-        <button onClick={kakaologin}>카카오</button>
-        <button onClick={googlelogin}>구글</button>
+      <div style={{ position: 'fixed', top: '50px', left: '50px', fontSize: '40px' }}>
+        <div style={{ fontFamily: 'iceJaram-Rg' }}>스물다섯 여든하나</div>
+        <div style={{ fontSize: '15px', marginTop: '10px', marginRight: '60px' }}>
+          특별한 인연을 만드는 공간, 여러분을 기다리는 인생 선배분들과 함께 보내봅시다.
+        </div>
+      </div>
+      {/*<img style={{ height: '100vh' }} src="/test/landing.png" alt="image" />*/}
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div
+          style={{
+            width: '80%',
+            height: '20%',
+            position: 'fixed',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            bottom: '50px',
+          }}
+        >
+          <button
+            style={{
+              width: '100%',
+              height: '40%',
+              marginTop: '10px',
+              backgroundColor: '#fee500',
+              borderRadius: '5px',
+              border: '1px solid darkgray',
+            }}
+            onClick={kakaologin}
+          >
+            <div style={{ display: 'flex', width: '100%' }}>
+              <img style={{ width: '8%', marginLeft: '20px' }} src="/login/kakao.png" />
+              <div style={{ width: '90%', margin: 'auto' }}>카카오로 시작하기</div>
+            </div>
+          </button>
+
+          <button
+            style={{
+              width: '100%',
+              height: '40%',
+              marginTop: '5px',
+              backgroundColor: 'white',
+              borderRadius: '5px',
+              border: '1px solid darkgray',
+            }}
+            onClick={googlelogin}
+          >
+            <div style={{ display: 'flex', width: '100%' }}>
+              <img style={{ width: '9%', marginLeft: '20px' }} src="/login/google.png" />
+              <div style={{ width: '90%', margin: 'auto' }}>구글로 시작하기</div>
+            </div>
+          </button>
+        </div>
       </div>
     </div>
   );
