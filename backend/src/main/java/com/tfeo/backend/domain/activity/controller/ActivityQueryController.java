42f918de2b875fe5b2093c0531b03ef6df91602f
문준형 <moonabcd@naver.com>
2024-04-01 16:27:04 +0900

    feat : activity memberNo적용
    Former-commit-id: 9fe7fc76fff6c475b0da7e14e9819fd4782680c7

--- backend/src/main/java/com/tfeo/backend/domain/activity/controller/ActivityQueryController.java
+++ backend/src/main/java/com/tfeo/backend/domain/activity/controller/ActivityQueryController.java
@@ -1,8 +1,9 @@
 package com.tfeo.backend.domain.activity.controller;
 
-import static com.tfeo.backend.common.model.type.Role.*;
-
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,10 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tfeo.backend.common.model.dto.SuccessResponse;
-import com.tfeo.backend.common.model.type.Role;
+import com.tfeo.backend.common.service.AuthenticationService;
 import com.tfeo.backend.domain.activity.model.dto.ReadActivityRequestDto;
 import com.tfeo.backend.domain.activity.model.dto.ReadActivityResponseDto;
 import com.tfeo.backend.domain.activity.service.ActivityQueryService;
+import com.tfeo.backend.domain.member.model.entity.Member;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,32 +32,48 @@ import lombok.RequiredArgsConstructor;
 public class ActivityQueryController {
 
 	private final ActivityQueryService activityQueryService;
+	private final AuthenticationService authenticationService;
 
 	//활동인증글 목록 검색
 	@GetMapping
-	public ResponseEntity<?> activityListRead(
-		@ModelAttribute("searchRequest") ReadActivityRequestDto request,
-		@PageableDefault(size = 5, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+	public ResponseEntity<?> activityListRead(@ModelAttribute("searchRequest") ReadActivityRequestDto request,
+		@PageableDefault(size = 5, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+		HttpServletRequest httprequest
 	) {
-		Long memberNo = 1L;
-		Page<ReadActivityResponseDto> result = activityQueryService.readActivityList(memberNo,  request, pageable);
+		Optional<Member> memberOptional = authenticationService.getMember(httprequest);
+		if (!memberOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("가입된 사용자 데이터를 찾을 수 없습니다.");
+		}
+		Member member = memberOptional.get();
+		Long memberNo = member.getMemberNo();
+		Page<ReadActivityResponseDto> result = activityQueryService.readActivityList(memberNo, request, pageable);
 		return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, "활동인증글 목록 조회 성공", result));
 	}
 
 	//학생별 로드맵 조회
 	@GetMapping(value = "/{studentNo}")
-	public ResponseEntity<?> roadmapRead(@PathVariable("studentNo") Long studentNo) {
-		Long memberNo = 1L;
-		List<ReadActivityResponseDto> result = activityQueryService.readRoadmap(memberNo,  studentNo);
+	public ResponseEntity<?> roadmapRead(@PathVariable("studentNo") Long studentNo, HttpServletRequest httprequest) {
+		Optional<Member> memberOptional = authenticationService.getMember(httprequest);
+		if (!memberOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("가입된 사용자 데이터를 찾을 수 없습니다.");
+		}
+		Member member = memberOptional.get();
+		Long memberNo = member.getMemberNo();
+		List<ReadActivityResponseDto> result = activityQueryService.readRoadmap(memberNo, studentNo);
 		return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, "로드맵 조회 성공", result));
 	}
 
 	//활동인증글 상세 조회
 	@GetMapping(value = "/{activityNo}/detail")
-	public ResponseEntity<?> activityRead(@PathVariable("activityNo") Long activityNo) {
-		Long memberNo = 1L;
+	public ResponseEntity<?> activityRead(@PathVariable("activityNo") Long activityNo, HttpServletRequest httprequest) {
+		Optional<Member> memberOptional = authenticationService.getMember(httprequest);
+		if (!memberOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("가입된 사용자 데이터를 찾을 수 없습니다.");
+		}
+		Member member = memberOptional.get();
+		Long memberNo = member.getMemberNo();
 
-		ReadActivityResponseDto result = activityQueryService.readActivity(memberNo,  activityNo);
+		ReadActivityResponseDto result = activityQueryService.readActivity(memberNo, activityNo);
 		return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, "활동인증글 상세 조회 성공", result));
 	}
 
