060d6d5b8ccb6c1c2dda7cde57fe3077e1abc193
문준형 <moonabcd@naver.com>
2024-04-01 16:26:46 +0900

    feat : review memberNo적용
    Former-commit-id: 9e78c2e6f2b66b90cee18b816d6fd4889a761872

--- backend/src/main/java/com/tfeo/backend/domain/review/controller/ReviewQueryController.java
+++ backend/src/main/java/com/tfeo/backend/domain/review/controller/ReviewQueryController.java
@@ -3,6 +3,9 @@ package com.tfeo.backend.domain.review.controller;
 import static com.tfeo.backend.common.model.type.Role.*;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tfeo.backend.common.model.dto.SuccessResponse;
 import com.tfeo.backend.common.model.type.Role;
+import com.tfeo.backend.common.service.AuthenticationService;
+import com.tfeo.backend.domain.member.model.entity.Member;
 import com.tfeo.backend.domain.review.model.dto.ReadReviewResponseDto;
 import com.tfeo.backend.domain.review.service.ReviewQueryService;
 
@@ -24,11 +29,17 @@ import lombok.RequiredArgsConstructor;
 public class ReviewQueryController {
 
 	private final ReviewQueryService reviewQueryService;
+	private final AuthenticationService authenticationService;
 
 	//리뷰 목록 조회
 	@GetMapping(value = "/{homeNo}")
-	public ResponseEntity<?> reviewListRead(@PathVariable("homeNo") Long homeNo) {
-		Long memberNo = 1L;
+	public ResponseEntity<?> reviewListRead(@PathVariable("homeNo") Long homeNo, HttpServletRequest httprequest) {
+		Optional<Member> memberOptional = authenticationService.getMember(httprequest);
+		if (!memberOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("가입된 사용자 데이터를 찾을 수 없습니다.");
+		}
+		Member member = memberOptional.get();
+		Long memberNo = member.getMemberNo();
 		Role role = USER;
 		List<ReadReviewResponseDto> result = reviewQueryService.readReviewList(memberNo, role, homeNo);
 		return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, "리뷰 목록 조회 성공", result));
@@ -37,8 +48,13 @@ public class ReviewQueryController {
 
 	//리뷰 상세 조회
 	@GetMapping(value = "/detail/{reviewNo}")
-	public ResponseEntity<?> reviewRead(@PathVariable("reviewNo") Long reviewNo) {
-		Long memberNo = 1L;
+	public ResponseEntity<?> reviewRead(@PathVariable("reviewNo") Long reviewNo, HttpServletRequest httprequest) {
+		Optional<Member> memberOptional = authenticationService.getMember(httprequest);
+		if (!memberOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("가입된 사용자 데이터를 찾을 수 없습니다.");
+		}
+		Member member = memberOptional.get();
+		Long memberNo = member.getMemberNo();
 		Role role = USER;
 		ReadReviewResponseDto result = reviewQueryService.readReview(memberNo, role, reviewNo);
 		return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, "리뷰 상세 조회 성공", result));
