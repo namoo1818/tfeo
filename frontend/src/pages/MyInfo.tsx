144e7700bde399a1ff7283512af34f205c3c8281
김태윤 <whddn_0426@naver.com>
2024-04-01 09:26:49 +0900

    refactor: 내 정보 페이지 수정 중
    Former-commit-id: fea6daa539d5effede43899935ebb0ef90313014

--- frontend/src/pages/MyInfo.tsx
+++ frontend/src/pages/MyInfo.tsx
@@ -7,20 +7,25 @@ import axios from 'axios';
 import SearchAddress from '../components/mypage/SearchAddress';
 import { IMember } from '../interfaces/MemberInterface';
 import { getFileFromS3 } from '../api/S3Apis';
+import { getDetail, getRoadAddress, getRoadNameAddress } from '../utils/addressUtils';
+import { IAddress } from '../interfaces/AddressInterface';
 
 const MyInfo: React.FC = () => {
   const [isEditing, setIsEditing] = useState(false);
   const [open, setOpen] = useState(false);
-  const [newMemberInfo, setNewMemberInfo] = useState<IMember>();
+  const [newMemberInfo, setNewMemberInfo] = useState<IMember>({} as IMember);
   const { setMemberState, setAddressState, MemberInfo, AddressInfo } = useMemberStore();
 
   // 프로필 업로드
   const [profileImage, setProfileImage] = useState('');
+  const [isProfileImageUploaded, setIsProfileImageUploaded] = useState<boolean>(false);
   //재학증명서 업로드
   const [ceritifcate, setCertificate] = useState('');
+  const [isCertificateUploaded, setIsCertificateUploaded] = useState<boolean>(false);
   const handleImageUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
       setProfileImage(URL.createObjectURL(event.target.files[0]));
+      setIsProfileImageUploaded(true);
     }
   };
 
@@ -75,17 +80,38 @@ const MyInfo: React.FC = () => {
       newMemberInfo.email,
       newMemberInfo.registerNo,
       newMemberInfo.address,
-      false,
-      false,
+      isProfileImageUploaded,
+      isCertificateUploaded,
     );
   };
 
-  const handleAddressChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
-    const { name, value } = event.target;
-    setAddressState({
-      ...AddressInfo,
-      [name]: value,
+  const handleAddressChange = (data: any) => {
+    setNewMemberInfo((prev) => ({
+      ...prev,
+      address: {
+        si: data.sido,
+        sgg: data.sigungu,
+        ro: data.roadname + ' ' + data.addressEnglish.split(',')[0],
+        emd: data.bname + ' ' + data.autoJibunAddressEnglish.split(',')[0],
+        detail: '',
+      },
+    }));
+  };
+  const handleAddressDetailChange = (
+    e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    const { id, value } = e.target;
+    if (!newMemberInfo) return;
+    setNewMemberInfo({
+      ...newMemberInfo,
+      address: {
+        ...newMemberInfo.address,
+        detail: value,
+      },
     });
+  };
+  const uploadCertificate = () => {
+    setIsCertificateUploaded(true);
   };
   if (!newMemberInfo) {
     return (
@@ -94,10 +120,6 @@ const MyInfo: React.FC = () => {
       </>
     );
   }
-  const showlog = () => {
-    console.log(newMemberInfo);
-  };
-
   return (
     <div
       className="wrap-container"
@@ -106,7 +128,6 @@ const MyInfo: React.FC = () => {
         if (open) setOpen(false);
       }}
     >
-      <button onClick={showlog}>콘솔찍기</button>
       <div className="profile-image-container">
         <label htmlFor="image-upload">
           <Avatar alt="Remy Sharp" src={profileImage} sx={{ width: 100, height: 100, marginTop: 5 }} />
@@ -151,7 +172,6 @@ const MyInfo: React.FC = () => {
             style={{ width: '150%', height: '2em', border: 'none', resize: 'none', whiteSpace: 'pre-line' }}
             defaultValue={MemberInfo.phone}
             onChange={handleMemberChange}
-            readOnly={!isEditing}
           />
           <br />
           <span>주민등록번호</span>
@@ -162,30 +182,32 @@ const MyInfo: React.FC = () => {
             style={{ width: '150%', height: '2em', border: 'none', resize: 'none', whiteSpace: 'pre-line' }}
             defaultValue={MemberInfo.registerNo}
             onChange={handleMemberChange}
-            readOnly={!isEditing}
           />
           <br />
           <span>주소</span>
           <br />
-          <SearchAddress open={open} handleModal={handleModal} />
+          <SearchAddress
+            open={open}
+            handleModal={handleModal}
+            newMemberInfo={newMemberInfo}
+            handleAddressChange={handleAddressChange}
+          />
           <textarea
             className={isEditing ? 'textarea-editing' : ''}
             id="address"
             style={{ width: '150%', height: '2em', border: 'none', resize: 'none', whiteSpace: 'pre-line' }}
-            defaultValue={`${MemberInfo.address.si} ${MemberInfo.address.sgg} ${MemberInfo.address.ro}`}
-            onChange={handleAddressChange}
-            readOnly={!isEditing}
+            value={getRoadAddress(newMemberInfo.address)}
           />
           <br />
           <span>상세 주소</span>
           <textarea
             style={{ width: '150%', height: '2em', border: 'none', resize: 'none', whiteSpace: 'pre-line' }}
-            defaultValue={MemberInfo.address.detail}
-            onChange={handleAddressChange}
-            readOnly={!isEditing}
+            value={getDetail(newMemberInfo.address)}
+            onChange={handleAddressDetailChange}
           />
         </div>
         <span>재학증명서</span>{' '}
+        {MemberInfo.certificate !== 'CERTIFICATED' && <input type="file" onChange={uploadCertificate} />}
         {MemberInfo.certificate === 'CERTIFICATED' && (
           <>
             <button style={{ border: '1px solid black ', borderRadius: '5px' }} onClick={() => null}>
